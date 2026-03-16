a21510a7b906b047411ad9d88b7c0f1758466a48
Giovanni <amaurymendezcpe@gmail.com>
2026-03-16 02:36:23 -0500

    new test2

--- TestComp/UnitTest1.cs
+++ TestComp/UnitTest1.cs
@@ -9,4 +9,11 @@ public class UnitTest1
         var valor = cuenta.Incrementar();
         Assert.Equal(valor,1);
     }
+    [Fact]
+    public void Test2()
+    {
+        Contador cuenta = new Contador();
+        var valor = cuenta.Decrementar();
+        Assert.Equal(valor,-1);
+    }
 }
